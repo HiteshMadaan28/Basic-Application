ee8e038f0e2a2f9540042ad58be8928da7089e86
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-02-13 14:44:54 +0530

    MAD work

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,18 +10,28 @@ import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
-    @Override
+    Button b1,b2;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //R means resource folder
         setContentView(R.layout.activity_main);
 
-        b1 = (Button) findViewById(R.id.button);
+        b1 = (Button) findViewById(R.id.button1);
+        b2 = (Button) findViewById(R.id.button2);
+
         b1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View vew){
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View vew){
+                Intent intent = new Intent(MainActivity.this, MainActivity3.class);
                 startActivity(intent);
             }
         });
